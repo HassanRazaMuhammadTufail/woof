7438b372fc72aacf27cfe2533b947ec69e297cb8
ahsan-ahmed <ahsan_ahmed01@yahoo.com>
2019-04-30 11:15:45 +0500

    ver-1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 import "./App.css";
+import SiderDemo from "./Components/Layout";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello Home</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello World Ahsan Ahmed
-          </a>
-        </header>
+        <SiderDemo />
       </div>
     );
   }
---
+++ src/Components/Layout/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./index.css";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import Button from "../Button";
+
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" style={{ cursor: "auto" }}>
+              <Icon type="user" />
+              <span style={{ color: "white" }}>Ahsan Ahmed</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="home" />
+              <span>Home</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="camera" />
+              <span>Camera</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Team 1</Menu.Item>
+              <Menu.Item key="6">Team 2</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            <div className="footer-div1">
+              <Button />
+            </div>
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default SiderDemo;
